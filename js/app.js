6d10da0936426235aa2c983d4493fc3669cd247e
Gerald <lanselot_hailander@hotmail.com>
2016-04-19 18:46:32 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var InventarioApp = require('./components/InventarioApp.react.js')
+
+ReactDOM.render(<InventarioApp />, document.getElementById('inventario-app'));
---
+++ js/components/InventarioApp.react.js
@@ -0,0 +1,11 @@
+var React = require("react");
+
+var InventarioApp = React.createClass({
+	render: function() {
+		return (
+			<h1>Hello World!</h1>
+		);
+	}
+});
+
+module.exports = InventarioApp;
