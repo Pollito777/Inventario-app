1677cee60877c19a32c12e7196aeb72494cb80eb
Gerald <lanselot_hailander@hotmail.com>
2016-04-21 16:45:50 -0400

    Formulario Login creado

--- js/components/InventarioApp.react.js
+++ js/components/InventarioApp.react.js
@@ -1,11 +1,14 @@
 var React = require("react");
-//var Header = require("./Header.react.js");
-//var Footer = require("./Footer.react.js");
 var AppBar = require('material-ui/lib/app-bar');
 var FlatButton = require('material-ui/lib/flat-button');
 var LeftNav = require('material-ui/lib/left-nav');
 var MenuItem = require('material-ui/lib/menus/menu-item');
 
+var Card = require('material-ui/lib/card/card');
+var CardActions = require('material-ui/lib/card/card-actions');
+var CardMedia = require('material-ui/lib/card/card-media');
+var FlatButton = require('material-ui/lib/flat-button');
+var TextField = require('material-ui/lib/text-field');
 
 var InventarioApp = React.createClass({
 	getInitialState: function () {
@@ -22,15 +25,33 @@ var InventarioApp = React.createClass({
 	render: function() {
 		return (
 			<div>
-			<AppBar
-    			title="InventarioApp"
-    			onLeftIconButtonTouchTap={this._handleMenu}
-    			iconElementRight={<FlatButton label="Salir" />}
-  			/>
-  			<LeftNav open={this.state.open}>
-       		    <MenuItem>Menu Item</MenuItem>
-       	   		<MenuItem>Menu Item 2</MenuItem>
-        	</LeftNav>
+				<AppBar
+    				title="InventarioApp"
+    				onLeftIconButtonTouchTap={this._handleMenu}
+    				iconElementRight={<FlatButton label="Salir" />}
+  				/>
+  				<LeftNav
+	  				open={this.state.open}
+	  				docked={false}
+	  				onRequestChange={open => this.setState({open})}
+	  			>
+	       		    <MenuItem>Formulario 1</MenuItem>
+	       	   		<MenuItem>Formulario 2</MenuItem>
+	        	</LeftNav>
+	        	<div style={{textAlign:'center'}}>
+	        	<h2>Bienvenido a InventarioApp</h2>
+		    	<TextField
+			      hintText="Usuario"
+			      floatingLabelText="Usuario"
+			      
+			    /><br/>
+			    <TextField
+			      hintText="Password"
+			      floatingLabelText="Password`"
+			      type="password"
+			    /><br/>
+			    <FlatButton label="Login" color/>
+			    </div>
         	</div>
 		);
 	}
